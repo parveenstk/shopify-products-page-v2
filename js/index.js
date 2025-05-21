a34714e26b8c5dd6a276d4610e91d3ab711f37b3
parveenstk <parveen.kumar@suretekinfosoft.com>
2025-05-21 12:29:45 +0530

    buttons color selection done.

--- js/index.js
+++ js/index.js
@@ -1,36 +1,73 @@
-// to track which flavour is selected
-
+// Yomz Carousels
 const yomzProduct = document.getElementById('yomz-original-carousel');
 const soursProduct = document.getElementById('yomz-sours-carousel');
 
-const handleCheckboxClick = (clicked) => {
-    console.log("clicked:", clicked.value);
-    if (clicked.value === 'yomzSours') {
-        soursProduct.classList.add('show');
-        soursProduct.classList.remove('hide');
-        yomzProduct.classList.add('hide');
-    } else if (clicked.value === 'yomzOriginal') {
-        yomzProduct.classList.remove('hide');
-        yomzProduct.classList.add('show');
-        soursProduct.classList.add('hide');
-    }
+// Yomz buttons
+const originalButton = document.getElementById('original-Button');
+const soursButton = document.getElementById('sours-Button');
+const checkOriginal = document.getElementById('checkOriginal');
+const checkSour = document.getElementById('checkSour');
 
-    const checkboxes = document.querySelectorAll('input[name="productType"]');
-    checkboxes.forEach(cb => {
-        if (cb !== clicked) cb.checked = false;
-    });
-};
-
+// Next Step Function (showing next & back step)
 const yomzOriginal = document.getElementById('yomz-original');
 const yomzSours = document.getElementById('yomz-sours');
 
 const nextStep1 = document.getElementById('nextStep1');
 const nextStep2 = document.getElementById('nextStep2');
+const nextStep3 = document.getElementById('nextStep3');
 const backStep2 = document.getElementById('backStep2');
 const backStep3 = document.getElementById('backStep3');
 const offerOne = document.getElementById('offer-one');
 const offerTwo = document.getElementById('offer-two');
 const offerThree = document.getElementById('offer-three');
+
+const handleCheckboxClick = (clicked) => {
+    console.log("clicked:", clicked.value);
+
+    if (clicked.value === 'yomzSours') {
+        // Show sour
+        nextStep1.classList.add('buttonGreen');
+        nextStep2.classList.add('buttonGreen');
+        nextStep3.classList.add('buttonGreen');
+        checkOriginal.style.accentColor = 'black';
+        originalButton.classList.remove('borderBlue');
+        soursButton.classList.add('borderGreen');
+        soursProduct.classList.remove('hide');
+        soursProduct.classList.add('show');
+        yomzProduct.classList.add('hide');
+
+        // Refresh Swiper instance
+        if (swiperInstances['yomz-sours-carousel']) {
+            const { productSlider, productThumbs } = swiperInstances['yomz-sours-carousel'];
+            productSlider.update();
+            productThumbs.update();
+        }
+    } else if (clicked.value === 'yomzOriginal') {
+        // Show original
+        nextStep1.classList.remove('buttonGreen');
+        nextStep2.classList.remove('buttonGreen');
+        nextStep3.classList.remove('buttonGreen');
+        checkSour.style.accentColor = 'green';
+        soursButton.classList.remove('borderGreen');
+        originalButton.classList.add('borderBlue');
+        yomzProduct.classList.remove('hide');
+        yomzProduct.classList.add('show');
+        soursProduct.classList.add('hide');
+
+        // Refresh Swiper instance
+        if (swiperInstances['yomz-original-carousel']) {
+            const { productSlider, productThumbs } = swiperInstances['yomz-original-carousel'];
+            productSlider.update();
+            productThumbs.update();
+        }
+    }
+
+    // Uncheck other checkboxes
+    const checkboxes = document.querySelectorAll('input[name="productType"]');
+    checkboxes.forEach(cb => {
+        if (cb !== clicked) cb.checked = false;
+    });
+};
 
 // console.log(offerTwo);
 
